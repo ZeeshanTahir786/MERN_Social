b5cacc7d8e24f1bf4897b944c8575d8daf56cd79
Hafiz ZeeShan <hafizzeeshan138@gmail.com>
2022-02-10 15:45:34 +0500

    Implement Follow User Api

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -78,3 +78,57 @@ exports.login = async (req, res) => {
     });
   }
 };
+
+exports.folloeUser = async (req, res) => {
+  try {
+    const userToFollow = await User.findById(req.params.id);
+
+    const loggedInUser = await User.findById(req.user._id);
+    if (!userToFollow) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    // 1. Condition for avoiding double Followers
+    // if (loggedInUser.following.includes(userToFollow._id)) {
+    //   return res.status(403).json({
+    //     success: false,
+    //     message: "User already followed",
+    //   });
+    // }
+
+    // 2. Second Solution is On second Request Make it UnFollow
+    if (loggedInUser.following.includes(userToFollow._id)) {
+      const indexFollowing = loggedInUser.following.indexOf(userToFollow._id);
+      loggedInUser.following.splice(indexFollowing, 1);
+
+      const indexFollowers = userToFollow.followers.indexOf(loggedInUser._id);
+      userToFollow.followers.splice(indexFollowers, 1);
+
+      await loggedInUser.save();
+      await userToFollow.save();
+
+      return res.status(200).json({
+        success: true,
+        message: "User UnFollowed",
+      });
+    } else {
+      loggedInUser.following.push(userToFollow._id);
+      userToFollow.followers.push(loggedInUser._id);
+
+      await loggedInUser.save();
+      await userToFollow.save();
+
+      return res.status(200).json({
+        success: true,
+        message: "User Followed",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { register, login } = require("../controllers/user");
+const { register, login, folloeUser } = require("../controllers/user");
+const { isAuthenticated } = require("../middlewares/auth");
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
+router.route("/follow/:id").get(isAuthenticated, folloeUser);
 
 module.exports = router;
