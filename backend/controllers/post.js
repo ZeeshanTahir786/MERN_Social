881d5154b46bc0eb87ef207489a9bc3034ff91d4
Hafiz ZeeShan <hafizzeeshan138@gmail.com>
2022-02-10 15:08:02 +0500

    Implement Like & Dislike Api

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -30,9 +30,93 @@ exports.createPost = async (req, res) => {
   }
 };
 
+exports.deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    if (post.owner.toString() !== req.user._id.toString()) {
+      return res.status(403).json({
+        success: false,
+        message: "Unautherized User",
+      });
+    }
+    await post.remove();
+
+    const user = await User.findById(req.user._id);
+
+    const index = await user.posts.indexOf(req.params._id);
+    user.posts.splice(index, 1);
+    await user.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Post deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 exports.likeAndUnlikePost = async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    if (post.likes.includes(req.user._id)) {
+      const index = post.likes.indexOf(req.user._id);
+      post.likes.splice(index, 1);
+      await post.save();
+      return res.status(200).json({
+        success: true,
+        message: "Post Unliked",
+      });
+    } else {
+      post.likes.push(req.user._id);
+      await post.save();
+      return res.status(200).json({
+        success: true,
+        message: "Post Liked",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    if (!posts) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      results: posts.length,
+      posts,
+    });
   } catch (error) {
     res.status(500).json({
       success: false,
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,8 +1,20 @@
 const express = require("express");
-const { createPost } = require("../controllers/post");
+const {
+  createPost,
+  likeAndUnlikePost,
+  deletePost,
+  getAllPosts,
+} = require("../controllers/post");
 const { isAuthenticated } = require("../middlewares/auth");
 const router = express.Router();
 
 router.route("/post/upload").post(isAuthenticated, createPost);
+
+router
+  .route("/post/:id")
+  .get(isAuthenticated, likeAndUnlikePost)
+  .delete(isAuthenticated, deletePost);
+
+router.route("/all/posts").get(isAuthenticated, getAllPosts);
 
 module.exports = router;
